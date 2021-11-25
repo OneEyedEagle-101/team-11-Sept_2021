eabc67d8ea1d0d3d1925ff69cfc5257710de7746
Saurabh Patkowar <91302710+OneEyedEagle-101@users.noreply.github.com>
2021-11-25 13:23:29 +0530

    Add files via upload

---
+++ Assignment_4_DBMS.sql
@@ -0,0 +1,20 @@
+###Assignment 4###
+
+#Q1
+select onum,amt,Date_format(odate,'%b-%m-%Y')"Odate"from orders;
+
+#Q2
+select * from customers where snum='1001';
+
+#Q3
+select city, sname,snum,comm from salespeople;
+
+#Q4
+select cname,rating from customers;
+
+#Q5
+select distinct sname from salespeople;
+
+
+
+
